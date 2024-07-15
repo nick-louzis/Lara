9be77c235439b413e39060f81f3fdc3a4910561d
Maria Magdalini <momoloca42@gmail.com>
2024-07-15 10:32:53 +0300

    created posts controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function showCreatePost(){
+        return view('create-post');
+
+    }
+
+    public function createPost(Request $request){
+
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        //create the field in the array and the user id with help of global auth().
+        $incomingFields['user_id'] = auth()->id();
+
+        Post::create($incomingFields);
+
+        return $incomingFields['body'];
+
+    }
+}
