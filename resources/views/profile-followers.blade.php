c410de946f489f5d627e2ffd16db8963d7d36936
Maria Magdalini <momoloca42@gmail.com>
2024-08-06 11:41:22 +0300

    Getting the user data from SharedData via the View::class from userController

--- resources/views/profile-followers.blade.php
+++ resources/views/profile-followers.blade.php
@@ -1,7 +1,7 @@
 <x-profile :sharedData="$sharedData">
   <div class="list-group">
     @foreach ($followers as $follow)
-      <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
+      <a href="/profile/{{$follow->userFollows->username}}" class="list-group-item list-group-item-action">
         <img class="avatar-tiny" src="{{$follow->userFollows->avatar}}" />
         {{$follow->userFollows->username}}
       </a>
